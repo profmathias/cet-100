4d443d00b30475f2f378b237fbb33493f8731f1c
mathiasbrito <mathias.brito@me.com>
2021-05-11 08:13:11 -0300

    código exemplo

--- Codigo/async-com-javascript/exemplo6.mjs
+++
@@ -1,7 +0,0 @@
-function esperar(tempo) {
-  return new Promise(((resolve, reject) => setTimeout(resolve, tempo)));
-}
-
-esperar(5000).then(() => {console.log("Espera Encerrada!"); return esperar(5000)})
-  .then(() => console.log("Segundo then!"));
-console.log("Aguardando Promise ser resolvida!")
--- Codigo/async-com-javascript/exemplo7.mjs
+++
@@ -1,9 +0,0 @@
-async function soma(a, b) {
-  return a + b;
-}
-
-const result = soma(10, 20);
-console.log(result instanceof Promise);
-
-result.then((value) => console.log("O resultado é: " + value));
-
